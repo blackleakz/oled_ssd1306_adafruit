631676b08a4575f59a91be4f84bb9f74bc53101e
Calvin Ronksley <45170098+blackleakz@users.noreply.github.com>
2023-08-11 05:20:00 +0200

    update

--- git.ps1
+++ git.ps1
@@ -1,9 +1,11 @@
 Import-Module posh-git
 
-
+$exclude="git config --global core.excludesFile .gitignore"
 $add="git add . "
-$cmi='git commit -a -m "added"'
+$cmi='git commit -a -m ".added"'
 $psh="git push origin main"
+
+powershell.exe -Command $exclude
 powershell.exe -Command $add
 powershell.exe -Command $cmi
 powershell.exe -Command $psh
